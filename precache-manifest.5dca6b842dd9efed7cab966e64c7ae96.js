9178191cb00043587445787eb08870918fa6a916
samatdav <samatdav@gmail.com>
2018-12-17 06:38:28 +0900

    Updates

---
+++ precache-manifest.5dca6b842dd9efed7cab966e64c7ae96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b0425b723b76ed254a",
+    "url": "/app4/static/css/main.de1fea91.chunk.css"
+  },
+  {
+    "revision": "a0b0425b723b76ed254a",
+    "url": "/app4/static/js/main.a0b0425b.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/app4/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c27dbc6ea380653bbe4e",
+    "url": "/app4/static/js/runtime~main.c27dbc6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app4/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "80196bb6d11f7b68427caf0a7e1b3d08",
+    "url": "/app4/index.html"
+  }
+];
